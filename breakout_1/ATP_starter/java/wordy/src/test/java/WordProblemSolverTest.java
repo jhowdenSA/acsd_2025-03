bf7de3706e33ba319b2ffcf131e752dedc9201d3
Paul Moore <pdmoore@gmail.com>
2025-03-23 21:37:53 -0700

    breakout1 - java - update gradle & test to junit 5.8.1

--- breakout_1/ATP_starter/java/wordy/src/test/java/WordProblemSolverTest.java
+++ breakout_1/ATP_starter/java/wordy/src/test/java/WordProblemSolverTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WordProblemSolverTest {
 
