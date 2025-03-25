a0ca60ac404afab5e03d67f93a61c3b46dba0abf
Jenise Howden <130766195+jhowdenSA@users.noreply.github.com>
2025-03-25 16:46:39 -0400

    Add code for wordy

--- breakout_2/ATP_starter/js-jest/wordy/wordy.js
+++ breakout_2/ATP_starter/js-jest/wordy/wordy.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const answer = () => {
-  throw new Error('Remove this statement and implement this function');
+export const answer = (question) => {
+    var strArray = question.split(" ");
+    var totalValue = 0;
+    var currentOperation;
+    for (var i = 2; i < strArray.length; i++) {
+        if (i % 2 === 0) {
+            strArray[i] = strArray[i].replace("?", "");
+            return parseInt(strArray[i]);
+        } else {
+        }
+    }
 };
