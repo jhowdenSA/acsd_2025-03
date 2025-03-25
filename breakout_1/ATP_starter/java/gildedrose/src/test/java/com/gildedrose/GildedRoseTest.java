46bd74f04438bde46f26a7cfeeeec1619ef7cd56
MrDice <accounts@christiandeiss.com>
2025-03-25 10:44:03 -0700

    test: added basic characterization tests

--- breakout_1/ATP_starter/java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ breakout_1/ATP_starter/java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,19 +3,51 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GildedRoseTest {
 
     @Test
-    public void this_test_needs_a_better_name() {
+    public void test_if_given_name_persists_after_updating_quality() {
         // Arrange
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose sut = new GildedRose(items);
+        GildedRose sut = createGildedRoseWithItem(0, 0);
 
         // Act
         sut.updateQuality();
 
         // Assert
-        assertEquals("fixme", sut.items[0].name);
+        assertEquals("foo", sut.items[0].name);
     }
+
+    @Test
+    public void test_if_quality_decreases_twice_as_fast_after_sellin_is_zero(){
+        GildedRose sut = createGildedRoseWithItem(0, 10);
+
+        sut.updateQuality();
+
+        assertEquals(8,  sut.items[0].quality);
+    }
+
+    private static GildedRose createGildedRoseWithItem(int sellIn, int quality) {
+        Item[] items = new Item[] { new Item("foo", sellIn, quality) };
+        GildedRose sut = new GildedRose(items);
+        return sut;
+    }
+
+    @Test
+    public void test_if_quality_decreases_once_at_end_of_day(){
+        GildedRose sut = createGildedRoseWithItem(5, 10);
+
+        sut.updateQuality();
+
+        assertEquals(9,  sut.items[0].quality);
+    }
+
+//    @Test
+//    public void test_if_quality_is_not_greater_than_fifty(){
+//        GildedRose sut = createGildedRoseWithItem(5, 100);
+//        sut.updateQuality();
+//        assertEquals( 50, sut.items[0].quality, "Quality should be less than 50");
+//    }
+
 }
